8cf6f0956a48cd2bed14bcc9733c1dc1c6873eca
Minakuchi0245 <kamilia3791@gmail.com>
2022-05-19 01:16:37 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,29 @@ class Item < ApplicationRecord
   has_one :purchase
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :shipping_area
+  belongs_to :shipping_days
+
+  with_options presence: true do
+    validates :item_name
+    validates :item_instruction
+    validates :category_id
+    validates :condition_id 
+    validates :shipping_charge_id
+    validates :shipping_area_id
+    validates :shipping_days_id
+    validates :price
+  end
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_charge_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_area_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_days_id, numericality: { other_than: 1, message: "can't be blank"}
+
 
 end
