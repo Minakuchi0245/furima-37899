804e322600e03efdc387e0b9ce1f21e9b79d7fb7
Minakuchi0245 <kamilia3791@gmail.com>
2022-05-19 20:15:00 +0900

    Revert "Merge pull request #16 from Minakuchi0245/商品出品機能"
    This reverts commit 10054ef0bc365d33a6c74531880e6e020b503e6f, reversing
    changes made to 9a3d57db83b6ddc8554b8b81065ddc317e0fa3a4.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,4 @@
 class ItemsController < ApplicationController
   def index
   end
-
-  #private
-  #def item_params
-  #  params.require(:item).permit(:item_instruction, :category_id, :condition_id, :shipping_charge_id, :shipping_area_id, :shipping_days_id, :price , :user_id, :image).merge(user_id: current_user.id)
-  #end
-
-
 end
