f479fad00c966709bb7438fb4b200909b8c10840
Minakuchi0245 <kamilia3791@gmail.com>
2022-05-19 19:22:54 +0900

    バリデーション、テストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,6 +10,17 @@ RSpec.describe Item, type: :model do
       it 'item_nameと,item_instruction,category_id,condition_id,shipping_charge_id,shipping_area_id,shipping_days_id,price,imageが存在すれば登録できる' do
         expect(@item).to be_valid
       end
+
+      it 'priceが半角数値、300以上、9,999,999以下であれば登録できる' do
+        @item.price = '300'
+        expect(@item).to be_valid
+      end
+
+      it 'priceが半角数値、300以上、9,999,999以下であれば登録できる' do
+        @item.price = '9999999'
+        expect(@item).to be_valid
+      end
+
     end
 
     context '商品出品がうまくいかない時' do
@@ -59,6 +70,24 @@ RSpec.describe Item, type: :model do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it 'priceが全角だと登録できない' do
+        @item.price = '１０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it 'priceが300以下だと登録できない' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+
+      it 'priceが9,999,999以上だと登録できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
 
       it 'imageが空だと登録できない' do
