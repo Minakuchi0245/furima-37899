8d3394e8601eef017512252c13186e096372d9f1
Minakuchi0245 <kamilia3791@gmail.com>
2022-05-24 00:21:55 +0900

    エラーが表示されるようにする

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -15,7 +15,7 @@ class PurchaseShippingAddress
   end
 
   validates :prefectures_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :postal_codeformat, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
   validates :telephone, format: { with: /\A[0-9]+\z/, message: "can't be blank" }
 
   def save
