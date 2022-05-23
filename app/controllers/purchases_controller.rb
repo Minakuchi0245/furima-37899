f1fff23c3a42ce7fa808deff0a3564168dfa7cb3
Minakuchi0245 <kamilia3791@gmail.com>
2022-05-23 23:58:06 +0900

    購入時に複数のテーブルへ情報が保存できるようにする(エラー、テストコード以外)

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,28 @@
+class PurchasesController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
+  def index
+    @item = Item.find(params[:item_id])
+    @purchase_shipping_address = PurchaseShippingAddress.new
+  end
+  
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_shipping_address = PurchaseShippingAddress.new(purchase_params)
+
+    if @purchase_shipping_address.valid?
+      @purchase_shipping_address.save
+      redirect_to root_path 
+    else
+      render :index
+    end
+
+  end
+  
+  private
+  
+    def purchase_params
+      params.permit(:shipping_address).permit(:postal_code, :prefectures_id, :city, :address, :building_name, :telephone).merge(user_id: current_user.id, item_id: @item.id)
+    end
+  
+end
