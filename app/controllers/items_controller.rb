8c81d155105faaede09d69939c4945b93174b2c4
Minakuchi0245 <kamilia3791@gmail.com>
2022-05-23 16:51:08 +0900

    before_action :set_item　追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, only: :edit
+  before_action :set_item,only: [:edit, :show, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -21,16 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
-
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -49,4 +46,9 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
